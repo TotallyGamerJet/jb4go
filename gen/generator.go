2733b84a7dd74b671c11db31d6577fd898f3e12c
TotallyGamerJet <totallygamerjet@gmail.com>
2021-05-11 20:05:52 -0400

    embed template
    embeds the template generator

--- gen/generator.go
+++ gen/generator.go
@@ -1,13 +1,18 @@
+// Package gen generates go files
 package gen
 
 import (
+	_ "embed"
 	"github.com/totallygamerjet/jb4go/transformer"
 	"io"
 	"text/template"
 )
 
+//go:embed go.gotmpl
+var gen string
+
 func Generate(g transformer.GoFile, w io.Writer) error {
-	t, err := template.ParseFiles("./gen/go.gotmpl")
+	t, err := template.New("jb2go").Parse(gen)
 	if err != nil {
 		return err
 	}
